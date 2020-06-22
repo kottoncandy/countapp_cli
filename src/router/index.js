037a6de1c1f021eddf54855250e6f93a6a07f6bc
kottoncandy <k.candy@gmail.com>
2020-06-22 10:03:06 +0900

    ページ構築

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -17,7 +17,26 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+
+  {
+    path: '/count',
+    name: 'Count',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Count.vue')
+  },
+
+  {
+    path: '/Score',
+    name: 'Score',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Score.vue')
   }
+
 ]
 
 const router = new VueRouter({
